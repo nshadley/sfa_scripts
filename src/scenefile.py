1d990748eb8c9472a2fb08f3e4b9f7b2159e19fc
nshadley <nmshadley@gmail.com>
2021-03-06 16:06:59 -0600

    Added path library

--- src/scenefile.py
+++ src/scenefile.py
@@ -1,7 +1,10 @@
+from pathlib import Path
+
+
 class SceneFile(object):
     """An abstract representation of a Scene File."""
     def __init__(self, folder_path, descriptor, task, ver, ext):
-        self.folder_path = folder_path
+        self.folder_path = Path(folder_path)
         self.descriptor = descriptor
         self.task = task
         self.ver = ver
@@ -12,6 +15,10 @@ class SceneFile(object):
         pattern = "{descriptor}_{task}_v{ver:03d}{ext}"
         return pattern.format(descriptor=self.descriptor, task=self.task,
                               ver=self.ver, ext=self.ext)
+
+    @property
+    def path(self):
+        return self.folder_path / self.filename
 
 
 scene_file = SceneFile("D:\\", "tank", "model", 1, ".ma")
